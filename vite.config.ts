efdfa5ec52cfa6552d7775c6690c65e5576ec70b
George Thompson <gwhthompson@gmail.com>
2025-12-23 14:39:24 +0000

    style: fix trailing commas in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,14 +10,14 @@ export default defineConfig({
     rollupOptions: {
       output: {
         format: 'esm',
-        entryFileNames: 'index.js',
-      },
-    },
+        entryFileNames: 'index.js'
+      }
+    }
   },
   ssr: {
     // Target Node.js - allows Node built-ins to be externalized
     target: 'node',
     // Bundle ALL npm dependencies (enables tree-shaking)
-    noExternal: true,
-  },
+    noExternal: true
+  }
 })
